9bdac11c9a0e614df1d8b7beaf1f64cb5501f94f
Marcos Junior <mod.gluguer@gmail.com>
2020-04-23 22:03:58 -0300

    Projetos ClassLibrary e api criados

---
+++ JujuControl.Business/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace JujuControl.Business
+{
+    public class Class1
+    {
+    }
+}
---
+++ JujuControl.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace JujuControl.Data
+{
+    public class Class1
+    {
+    }
+}
